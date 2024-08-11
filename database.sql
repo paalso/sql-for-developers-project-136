98a5c3909b0cb84c45fc9c8b4228eb88bd1b4e40
Pavel Soroka <40a.pvl@gmail.com>
2024-08-11 15:54:56 +0300

    Fix password field name in users table

--- database.sql
+++ database.sql
@@ -59,7 +59,7 @@ CREATE TABLE users(
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
     teaching_group_id BIGINT REFERENCES teaching_groups(id) ON DELETE
     SET
         NULL,
