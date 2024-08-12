9c3db67dd67f5e24744556ceb8367f7411b4a272
Pavel Soroka <40a.pvl@gmail.com>
2024-08-12 14:56:10 +0300

    Add tables enrollments, payments, program_completions, certificates

--- database.sql
+++ database.sql
@@ -60,10 +60,48 @@ CREATE TABLE users(
     username VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
-    teaching_group_id BIGINT REFERENCES teaching_groups(id) ON DELETE
-    SET
-        NULL,
-        role VARCHAR(50) CHECK (role IN ('student', 'teacher', 'admin')) NOT NULL,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    role VARCHAR(10) CHECK (role IN ('student', 'teacher', 'admin')) NOT NULL,
+    teaching_group_id BIGINT REFERENCES teaching_groups(id) ON DELETE SET NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE enrollments(
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    program_id BIGINT REFERENCES programs(id) ON DELETE SET NULL,
+    user_id BIGINT REFERENCES users(id) ON DELETE SET NULL,
+    status VARCHAR(10) CHECK (status IN ('active', 'pending', 'cancelled', 'completed')) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE payments(
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    enrollment_id BIGINT REFERENCES enrollments(id) ON DELETE SET NULL,
+    amount NUMERIC(10, 2),
+    status VARCHAR(10) CHECK (status IN ('pending', 'paid', 'failed', 'refunded')) NOT NULL,
+    payment_date DATE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE program_completions(
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    user_id BIGINT REFERENCES users(id) ON DELETE SET NULL,
+    program_id BIGINT REFERENCES programs(id) ON DELETE SET NULL,
+    status VARCHAR(10) CHECK (status IN ('active', 'completed', 'pending', 'cancelled')) NOT NULL,
+    start_date DATE,
+    end_date DATE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE certificates(
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    user_id BIGINT REFERENCES users(id) ON DELETE SET NULL,
+    program_id BIGINT REFERENCES programs(id) ON DELETE SET NULL,
+    certificate_url VARCHAR(255),
+    issue_date DATE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
