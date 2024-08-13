dbb43b61e1250a68f342577d0ee84104f0770e3b
Pavel Soroka <40a.pvl@gmail.com>
2024-08-13 21:43:21 +0300

    Replace fields like deleted_at -> is_deleted

--- database.sql
+++ database.sql
@@ -12,7 +12,7 @@ CREATE TABLE courses(
     description TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    deleted_at TIMESTAMP
+    is_deleted BOOLEAN DEFAULT FALSE NOT NULL
 );
 
 CREATE TABLE lessons(
@@ -24,7 +24,7 @@ CREATE TABLE lessons(
     course_position SMALLINT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    deleted_at TIMESTAMP
+    is_deleted BOOLEAN DEFAULT FALSE NOT NULL
 );
 
 CREATE TABLE modules(
@@ -33,7 +33,7 @@ CREATE TABLE modules(
     description TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    deleted_at TIMESTAMP
+    is_deleted BOOLEAN DEFAULT FALSE NOT NULL
 );
 
 CREATE TABLE programs(
